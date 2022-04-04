c89e39ebbd5bbe293a1aa8f054f1e468ccb3962c
stack72 <public@paulstack.co.uk>
2022-04-04 17:35:08 +0100

    Allowing case insensitive values for Enum validation

--- internal/tf/suppress/string.go
+++ internal/tf/suppress/string.go
@@ -4,7 +4,6 @@ import (
 	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 )
 
 func CaseDifference(_, old, new string, _ *schema.ResourceData) bool {
@@ -13,8 +12,6 @@ func CaseDifference(_, old, new string, _ *schema.ResourceData) bool {
 
 // CaseDifferenceV2Only only suppress case difference for v2.0.
 func CaseDifferenceV2Only(_, old, new string, _ *schema.ResourceData) bool {
-	if features.ThreePointOhBeta() {
-		return old == new
-	}
+	// FORK: Making the SKU case insensitive still to avoid breaking changes in Pulumi programs
 	return strings.EqualFold(old, new)
 }
